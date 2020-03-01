9896982690dd3ec5172de393447dafffa3ddc892
pfernc <61557085+pfernc@users.noreply.github.com>
2020-03-01 20:20:56 +0100

    Update Calcula_mes.c

--- Calcula_mes.c
+++ Calcula_mes.c
@@ -22,15 +22,33 @@ int main()
 	case 3:
 		printf("marzo\n");
 		break;
-	case 4:
+	case 4: 
+		printf("abril\n");
+		break;	
 	case 5:
+		printf("mayo\n");
+		break;	
 	case 6:
+		printf("junio\n");
+		break;	
 	case 7:
+		printf("julio\n");
+		break;	
 	case 8:
+		printf("agosto\n");
+		break;	
 	case 9:
+		printf("septiembre\n");
+		break;	
 	case 10:
+		printf("octubre\n");
+		break;	
 	case 11:
+		printf("noviembre\n");
+		break;	
 	case 12:
+		printf("diciembre\n");
+		break;	
 	default: printf("error");
 	}
 }
