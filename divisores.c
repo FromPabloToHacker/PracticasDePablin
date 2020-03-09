863c88d8d21d8d241344afbc100965732fce788a
pfernc <61557085+pfernc@users.noreply.github.com>
2020-03-09 13:20:29 +0100

    divisores hechos de manera optima a mi manera p7
    divisores hechos de manera optima a mi manera p7

---
+++ divisores.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int divisores();
+
+int main() {
+	
+	int divisores();
+	 
+	 int num1,i;
+	
+	printf("introduce un numero: \n");
+
+	scanf("%d",&num1);
+
+	for(i=1;i<=num1;i++)
+	
+	{
+		if(num1%i==0)
+		{
+	
+			printf("%d,es divisor de %d \n",i,num1);
+		}
+	}
+	}
